e3b5ed52052e52809bc5e4d83096c87ca5860f6e
tinhnguyen <tn@ipification>
2020-11-23 23:10:43 +0700

    revert

--- pages/_document.js
+++ pages/_document.js
@@ -4,93 +4,39 @@ import { ServerStyleSheets } from '@material-ui/core/styles';
 import theme from '../src/theme';
 
 export default class MyDocument extends Document {
-	render() {
-		return (
-			<Html lang="en">
-				<Head>
-					{/* PWA primary color */}
-					<meta name="theme-color" content={theme.palette.primary.main} />
-					{/* <link async as="font" href="/fonts/style.css" rel="stylesheet" /> */}
-					<link
-						rel="preload"
-						href="/fonts/fsdiavlomedium.woff2"
-						as="font"
-						type="font/woff2"
-						crossorigin="anonymous"
-					/>
-					<link
-						rel="preload"
-						href="/fonts/svn-larch-shaded.woff2"
-						as="font"
-						type="font/woff2"
-						crossorigin="anonymous"
-					/>
-					<link
-						rel="preload"
-						href="/fonts/helveticaneuemedium.woff2"
-						as="font"
-						type="font/woff2"
-						crossorigin="anonymous"
-					/>
-					<link
-						rel="preload"
-						href="/fonts/utm-caviar1.woff2"
-						as="font"
-						type="font/woff2"
-						crossorigin="anonymous"
-					/>
-					<link
-						rel="preload"
-						href="/fonts/utm-centurbold.woff2"
-						as="font"
-						type="font/woff2"
-						crossorigin="anonymous"
-					/>
-					<link
-						rel="preload"
-						href="/fonts/fsdiavlobold.woff2"
-						as="font"
-						type="font/woff2"
-						crossorigin="anonymous"
-					/>
-					<link
-						rel="preload"
-						href="/fonts/fsdiavlobook.woff2"
-						as="font"
-						type="font/woff2"
-						crossorigin="anonymous"
-					/>
-					<link
-						rel="preload"
-						href="/fonts/fa-solid-900.woff2"
-						as="font"
-						type="font/woff2"
-						crossorigin="anonymous"
-					/>
-				</Head>
-				<body>
-					<Main />
-					<NextScript />
-				</body>
-			</Html>
-		);
-	}
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          {/* PWA primary color */}
+          <meta name="theme-color" content={theme.palette.primary.main} />
+          <link async href="/fonts/style.css" rel="stylesheet" rel="preload" as="style"/>
+
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
 
 MyDocument.getInitialProps = async (ctx) => {
-	const sheets = new ServerStyleSheets();
-	const originalRenderPage = ctx.renderPage;
 
-	ctx.renderPage = () =>
-		originalRenderPage({
-			enhanceApp: (App) => (props) => sheets.collect(<App {...props} />)
-		});
+  const sheets = new ServerStyleSheets();
+  const originalRenderPage = ctx.renderPage;
 
-	const initialProps = await Document.getInitialProps(ctx);
+  ctx.renderPage = () =>
+    originalRenderPage({
+      enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+    });
 
-	return {
-		...initialProps,
-		// Styles fragment is rendered after the app and page rendering finish.
-		styles: [ ...React.Children.toArray(initialProps.styles), sheets.getStyleElement() ]
-	};
+  const initialProps = await Document.getInitialProps(ctx);
+
+  return {
+    ...initialProps,
+    // Styles fragment is rendered after the app and page rendering finish.
+    styles: [...React.Children.toArray(initialProps.styles), sheets.getStyleElement()],
+  };
 };
