6604feace580fa6d4edc462e70f029f9001bf2b6
tinhnguyen <tn@ipification>
2020-12-14 10:29:24 +0700

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -59,8 +59,8 @@ export default class MyDocument extends Document {
           />
         </Head>
         <body>
-        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-XXXX"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        {/* <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-XXXX"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript> */}
           <Main />
           <NextScript />
         </body>
