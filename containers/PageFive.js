35b0819e5e55a71806a69b931ecaee2e46f0a9bb
tinhnguyen <tn@ipification>
2020-12-15 09:56:57 +0700

    Update PageFive.js

--- containers/PageFive.js
+++ containers/PageFive.js
@@ -371,6 +371,7 @@ function PageFive() {
           {isSubmitting && <div style={{background: 'rgba(255, 255, 255, 0.5)', width: '100%', height:'100%', position:'absolute', top: "0", left:"0", display:'flex', justifyContent:'center', alignItems:'center'}}>
             <CircularProgress color="green" />
             </div>}
+            <br/>
       </div>
       ) : (
         <>
