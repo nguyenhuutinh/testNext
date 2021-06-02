78b4d4798879f1a75557e16db643474f3b79be79
tinhnguyen <tn@ipification>
2021-06-02 09:25:28 +0700

    Update PageOne.js

--- containers/PageOne.js
+++ containers/PageOne.js
@@ -1,26 +1,26 @@
-// import React from "react";
-// import { makeStyles } from "@material-ui/core/styles";
-// import Header from "./Header";
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Header from "./Header";
 
-// const useStyles = makeStyles((theme) => ({
-//   bgCover: {
-//     width: "100%",
-//   ["@media (max-width: 400px)"]: {
-//     width: "120%",
-//   },
-//   ["@media (max-width: 300px)"]: {
-//     width: "130%",
-//   },
-//   }
-// }));
+const useStyles = makeStyles((theme) => ({
+  bgCover: {
+    width: "100%",
+  ["@media (max-width: 400px)"]: {
+    width: "120%",
+  },
+  ["@media (max-width: 300px)"]: {
+    width: "130%",
+  },
+  }
+}));
 
-// function PageOne() {
-//   const classes = useStyles();
-//   return (
-//     <>
-//         <Header/>
-//     </>
-//   );
-// }
+function PageOne() {
+  const classes = useStyles();
+  return (
+    <>
+        <Header/>
+    </>
+  );
+}
 
-// export default PageOne;
+export default PageOne;
